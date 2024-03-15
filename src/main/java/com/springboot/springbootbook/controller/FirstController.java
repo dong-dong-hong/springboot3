37638fff387c3b451e38c2de8b09bd1cc6944fe4
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2024-03-15 22:33:52 +0900

    랜덤으로 명언 나오는 페이지

--- src/main/java/com/springboot/springbootbook/controller/FirstController.java
+++ src/main/java/com/springboot/springbootbook/controller/FirstController.java
@@ -33,4 +33,23 @@ public class FirstController {
         model.addAttribute("nickname", "슬러브");
         return "goodbye";
     }
+
+    @GetMapping("/random-quote")
+    public String randomQuote(Model model) {
+        String[] quotes = {
+                "행복은 습관이다. 그것을 몸에 지니라. " +
+                        "-허버드-" ,
+                "고개 숙이지 마십시오. 세상을 똑바로 정면으로 " +
+                        "바라보십시오. -헬렌 켈러-",
+                "고난의 시기에 동요하지 않는 것, 이것은 진정 " +
+                        "칭찬받을 만한 뛰어난 인물의 증거다. -베토벤-",
+                "당신이 할 수 있다고 믿든 할 수 업다고 믿든 " +
+                        "믿는 대로 될 것이다. -헨리 포드-",
+                "나의 죽음을 적들에게 알리지 마라. " +
+                        "-이순신-"
+        };
+        int randInt = (int) (Math.random() * quotes.length);
+        model.addAttribute("randomQuote", quotes[randInt]);
+        return "quote";
+    }
 }
